6b914806f63a6c27bb95ca9c03d5c341bc3594c2
Mc Rolls <mcrollsangeles@gmail.com>
2026-09-14 22:01:33 +0800

    fix build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 const SITE_URL = "https://mcrollsangeles.github.io";
 
 export default function sitemap(): MetadataRoute.Sitemap {
